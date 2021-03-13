3e7188db3c3165ee89b67a52df58c9994bab9ac6
hataevicha5922 <hataevicha5922@gmail.com>
2021-03-13 08:25:45 +0300

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+function getArr(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        res = res.concat(arr[i]);
+    }
+    console.log(res.sort());
 }
+
+module.exports = function towelSort(matrix) {
+    let res = [];
+    for (let i = 0; i < matrix.length; i++) {
+        res = res.concat(matrix[i]);
+    }
+    return res.sort(function (a, b) {
+        return a - b;
+    });
+};
