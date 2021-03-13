4241fe69a62c827d2cc7731564cd8f3aa4a904b9
hataevicha5922 <hataevicha5922@gmail.com>
2021-03-13 09:31:49 +0300

    fix: mistaike

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-function getArr(arr) {
-    let res = [];
-    for (let i = 0; i < arr.length; i++) {
-        res = res.concat(arr[i]);
-    }
-    console.log(res.sort());
-}
+// function getArr(arr) {
+//     let res = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         res = res.concat(arr[i]);
+//     }
+//     console.log(res.sort());
+// }
 
 module.exports = function towelSort(matrix) {
     let res = [];
-    for (let i = 0; i < matrix.length; i++) {
-        res = res.concat(matrix[i]);
+    if (arguments.length === 0) {
+        res = [];
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % 2 === 0) {
+                res = res.concat(matrix[i]);
+            } else {
+                res = res.concat(matrix[i].reverse());
+            }
+        }
     }
-    return res.sort(function (a, b) {
-        return a - b;
-    });
+    return res;
 };
